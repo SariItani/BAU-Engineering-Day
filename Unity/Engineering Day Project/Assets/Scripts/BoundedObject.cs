b2fe384fce495559bb05aad1d95c54742651e8fd
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-01-11 20:18:02 +0200

    clamping vertically and copying boundaries work now as well

--- Unity/Engineering Day Project/Assets/Scripts/BoundedObject.cs
+++ Unity/Engineering Day Project/Assets/Scripts/BoundedObject.cs
@@ -8,8 +8,6 @@ public class BoundedObject : MonoBehaviour
     public BoundedObject ref_obj;
     public float x_min;
     public float x_max;
-    public float y_min;
-    public float y_max;
     void Update()
     {
         if (ref_obj)
@@ -18,14 +16,11 @@ public class BoundedObject : MonoBehaviour
             // useful for copying defined bounds from the player or the camera.
             x_min = ref_obj.x_min;
             x_max = ref_obj.x_max;
-            y_min = ref_obj.y_min;
-            y_max = ref_obj.y_max;
         }
         float final_x = (x_max != 0) && (x_min != 0) ? Mathf.Clamp(target.position.x, x_min, x_max) : target.position.x;
-        float final_y = (y_max != 0) && (y_min != 0) ? Mathf.Clamp(target.position.y, y_min, y_max) : target.position.y;
         transform.position = new Vector3(
            final_x,
-           final_y,
+           transform.position.y,
            transform.position.z
         );
     }
--- Unity/Engineering Day Project/Assets/Scripts/FollowPlayer.cs
+++ Unity/Engineering Day Project/Assets/Scripts/FollowPlayer.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public class FollowPlayer : BoundedObject
 {
+    public float req_y;
 
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, req_y, transform.position.z);
+    }
 }
