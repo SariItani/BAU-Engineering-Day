5fdeaaa771b3776e2bb9ceee5c0b203b2cc08e3f
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-02 08:05:37 +0200

    added shit
    we need to re-implement the health bar

--- Unity/Engineering Day Project/Assets/Scripts/playerMovement.cs
+++ Unity/Engineering Day Project/Assets/Scripts/playerMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float speed;
-    bool isGrounded;
+    public Animator animator;
+    public Vector3 scaleChange;
+    bool isGrounded, isAttacking;
     public Transform groundCheck;
     public float groundDistance;
     public LayerMask groundMask;
@@ -22,9 +24,29 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("speed", speed);
+        animator.SetBool("attacking", isAttacking);
+        // animator.SetInt("combo", combo);
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
 
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0);
+        if (Input.GetAxisRaw("Horizontal") == -1f)
+        {
+            scaleChange = new Vector3(1f, 1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1f)
+        {
+            scaleChange = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            isAttacking = true;
+        }
+        else
+        {
+            isAttacking = false;
+        }
 
         if(isGrounded)
         {
@@ -35,6 +57,22 @@ public class playerMovement : MonoBehaviour
             //     rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
             // }
         }
+// Player speed determination
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 0.21f;
+        }
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            speed = 0.1f;
+        }
+        else
+        {
+            speed = 0.0f;
+        }
+// Player speed execution
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0);
+        transform.localScale = scaleChange;
     }
 
     private void OnDrawGizmos()
