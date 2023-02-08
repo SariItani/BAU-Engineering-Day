5c93d5b02ee76c3266f4bf36885148febf128ed7
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-08 11:49:33 +0200

    other enhancements
    jump glitch and animation fixed

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/playerMovement.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/playerMovement.cs
@@ -37,7 +37,7 @@ public class playerMovement : MonoBehaviour
         if(isGrounded)
         {
             Debug.Log("Ground Detected.");
-            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
             {
                 Debug.Log("Pressed W or Space");
                 rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
