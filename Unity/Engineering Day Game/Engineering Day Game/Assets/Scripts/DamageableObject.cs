3e21d596625d8ffa161b82edfd7b7a0a3c84c5ba
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-09 14:42:49 +0200

    Update DamageableObject.cs

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/DamageableObject.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/DamageableObject.cs
@@ -5,6 +5,9 @@ public class DamageableObject : MonoBehaviour
     public int maxhealth = 100;
     public int currentHealth;
     public bool invincible = false;
+    // player related
+    public GameOver gameOver;
+    public HealthBar healthBar;
 
     void Start()
     {
@@ -18,6 +21,16 @@ public class DamageableObject : MonoBehaviour
         if (currenthealth <= 0)
         {
             Die();
+        }
+        if (gameObject.tag == "Player")
+        {
+            healthBar.SetHealth(currentHealth);
+            if (currentHealth == 0)
+            {
+                // Call the game over scene if the game object destroyed has the tag "Player"
+                gameOver.EndGame();
+                // Destroy(gameObject);
+            }
         }
     }
 
