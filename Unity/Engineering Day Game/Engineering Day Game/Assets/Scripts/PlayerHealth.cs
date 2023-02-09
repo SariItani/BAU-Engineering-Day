43381166570f55abc09d6924282e0f4689e86974
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-09 15:47:36 +0200

    nothing much

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/PlayerHealth.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/PlayerHealth.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public DamageableObject damageableObject;
-    public GameOver gameOver;
+    // public GameOver gameOver;
     public HealthBar healthBar;
 
     void Update()
     {
         healthBar.SetHealth(damageableObject.currentHealth);
-        if (damageableObject.currentHealth == 0)
-        {
-            // Call the game over scene if the game object destroyed has the tag "Player"
-            gameOver.EndGame();
-            // Destroy(gameObject);
-        }
+        // if (damageableObject.currentHealth == 0)
+        // {
+        //     // Call the game over scene if the game object destroyed has the tag "Player"
+        //     gameOver.EndGame();
+        //     // Destroy(gameObject);
+        // }
     }
 }
