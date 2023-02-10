bab24fbf990819d2f5a9997fefb45d433db23ce3
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-10 18:58:19 +0200

    Update SettingsMenu.cs

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/SettingsMenu.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/SettingsMenu.cs
@@ -14,4 +14,8 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", volume);
     }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
 }
