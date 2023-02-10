946aeabf98b7581cd84f98d4d4c33db6781851ad
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-10 21:08:08 +0200

    pause menu

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/PauseMenu.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
     
-    void Resume()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1;
@@ -35,5 +36,15 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0;
         gameisPaused = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
