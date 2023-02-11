fb6f783d347c1498aa4554deb89f63353dadf0bc
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-11 14:29:04 +0200

    coffemachine

---
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/CoffeeMachine.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour
+{
+    private Animator animator;
+    private bool isMakingCoffee = false;
+
+    void Start()
+    {
+        animator = gameObject.Find("Coffee Machine").GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isMakingCoffee)
+        {
+            animator.SetTrigger("PlayerWantsCoffee");
+        }
+        else if ()
+        {
+
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+    }
+}
