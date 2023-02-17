f41afddce13707799d74e3a7580868a5a2470a52
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-17 18:06:20 +0200

    .
    plz help shahaz

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/EnemyIdle.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/EnemyIdle.cs
@@ -21,23 +21,32 @@ public class EnemyIdle : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         distance = (boss.transform.position - player.transform.position).x;
-        animator.SetFloat("distance", Math.abs(distance));
+        animator.SetFloat("distance", Mathf.Abs(distance));
         Debug.Log("Distance : " + distance);
         Vector3 flipped = boss.transform.localScale;
         flipped.z *= -1f;
-        if (boss.transform.position.x > player.transform.position.x && isFlipped)
-        {
-            // boss.transform.localScale = flipped;
-            // boss.transform.rotation = (0f, 180f, 0f);
-            boss.transform.Rotate(0f, 180f, 0f);
-            isFlipped = false;
-        }
-        if (boss.transform.position.x < player.transform.position.x && !isFlipped)
+        if (boss.transform.position.x > player.transform.position.x && !isFlipped)
         {
             // boss.transform.localScale = flipped;
             // boss.transform.rotation = (0f, 180f, 0f);
             boss.transform.Rotate(0f, 180f, 0f);
             isFlipped = true;
         }
+        if (boss.transform.position.x < player.transform.position.x && isFlipped)
+        {
+            // boss.transform.localScale = flipped;
+            // boss.transform.rotation = (0f, 180f, 0f);
+            boss.transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        Debug.Log("isFlipped: " + isFlipped);
+        Debug.Log("playerX : " + player.transform.position.x);
+        Debug.Log("bossX : " + boss.transform.position.x);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
     }
 }
--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/EnemyWalk.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/EnemyWalk.cs
@@ -8,7 +8,7 @@ public class EnemyWalk : StateMachineBehaviour
     GameObject boss;
     public float attackRange;
     float distance;
-    public bool isFlipped = true;
+    public bool isFlipped = false;
     Animator animator;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -23,31 +23,34 @@ public class EnemyWalk : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        distance = (boss.transform.position - player.transform.position).x;
-       animator.SetFloat("distance", Math.abs(distance));
+       animator.SetFloat("distance", Mathf.Abs(distance));
        Debug.Log("Distance : " + distance);
 // flipping
 // shahbaz idk what magic you do but plz flip the fucking dude
         Vector3 flipped = boss.transform.localScale;
         flipped.z *= -1f;
-        if (boss.transform.position.x > player.transform.position.x && isFlipped)
+        if (boss.transform.position.x > player.transform.position.x && !isFlipped)
         {
-            // boss.transform.localScale = flipped;
-            // boss.transform.rotation = (0f, 180f, 0f);
-            boss.transform.Rotate(0f, 180f, 0f);
-            isFlipped = false;
-        }
-        if (boss.transform.position.x < player.transform.position.x && !isFlipped)
-        {
-            // boss.transform.localScale = flipped;
+            boss.transform.localScale = flipped;
             // boss.transform.rotation = (0f, 180f, 0f);
             boss.transform.Rotate(0f, 180f, 0f);
             isFlipped = true;
+        }
+        if (boss.transform.position.x < player.transform.position.x && isFlipped)
+        {
+            boss.transform.localScale = flipped;
+            // boss.transform.rotation = (0f, 180f, 0f);
+            boss.transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
         }
 // transition to attack
         if (Vector2.Distance(player.transform.position, boss.transform.position) <= attackRange)
         {
             animator.SetTrigger("attack");
         }
+        Debug.Log("isFlipped: " + isFlipped);
+        Debug.Log("playerX : " + player.transform.position.x);
+        Debug.Log("bossX : " + boss.transform.position.x);
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
