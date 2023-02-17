7b2df6da62f072b800e44ba580f8ce0c0558b895
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-17 19:57:34 +0200

    Build alpha 1.0

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/AnimationEventListener.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/AnimationEventListener.cs
@@ -6,16 +6,20 @@ public class AnimationEventListener : MonoBehaviour
 {
     GameObject player;
 
+    AudioSource audiosource;
+
     public int attackRadius = 2;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        audiosource = gameObject.GetComponent<AudioSource>();
     }
     public void EventListener(int damage)
     {
         if (Vector2.Distance(transform.position, player.transform.position) <= attackRadius)
         {
+            audiosource.Play();
             player.GetComponent<DamageableObject>().TakeDamage(damage);
         }
     }
