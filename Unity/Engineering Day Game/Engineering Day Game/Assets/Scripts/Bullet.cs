3aaa13565147bb5f1b75d50d309e80aa9efceaf7
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-27 19:59:23 +0200

    made a thud sound for the bottles

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/Bullet.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/Bullet.cs
@@ -5,7 +5,7 @@ public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     public int bullet_damage = 25;
-
+    public AudioSource audioData;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     public Image item;
@@ -21,12 +21,14 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * speed;
         item.sprite = null;
         item.color = new Color(1f, 1f, 1f, 0f);
+        audioData = gameObject.GetComponent<AudioSource>();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<DamageableObject>() != null)
         {
             // Debug.Log("Enemy hit");
+            audioData.Play();
             other.GetComponent<DamageableObject>().TakeDamage(bullet_damage);
             Destroy(gameObject);
         }
