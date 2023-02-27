752f196ec9437575834d3a4d4323b7321d81fea4
SariItani <74187768+SariItani@users.noreply.github.com>
2023-02-27 21:20:40 +0200

    fixed the spawner

--- Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/Spawner.cs
+++ Unity/Engineering Day Game/Engineering Day Game/Assets/Scripts/Spawner.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+using Random = System.Random;
+
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
@@ -14,6 +16,7 @@ public class Spawner : MonoBehaviour
     public int maxSpawns;
     public float nextSpawn = 4f;
     private int spawns = 0;
+    Random random = new Random();
     // Start is called before the first frame update
     void Start()
     {
@@ -30,17 +33,17 @@ public class Spawner : MonoBehaviour
         if (Time.time > nextSpawn && ((spawns <= maxSpawns) || (maxSpawns == 0)))
         {
             nextSpawn = Time.time + spawnRate;
-            int randEnemy = Random.Range(0, BEAN.Length);
-            Debug.Log("randEnemy: " + randEnemy);
-            while (randEnemy == majorIndex)
-            {
-                randEnemy = Random.Range(0, BEAN.Length);
-                Debug.Log("randEnemy: " + randEnemy);
-            }
-            // int[] numbers = { 1, 3, 4, 9, 2 };
-            // int numToRemove = 4;
-            // numbers = numbers.Where(val => val != numToRemove).ToArray();
-            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+            // create array from 0 to majorIndex (non-inclusive)
+            int[] arr1 = Enumerable.Range(0, majorIndex).ToArray();
+            // create array from majorIndex + 1 to BEAN.length(basically 7)
+            int[] arr2 = Enumerable.Range(majorIndex + 1, BEAN.Length).ToArray();
+            // concat the two arrays and pick a randEnemy from the result
+            int[] range1 = arr1.Concat(arr2).ToArray();
+            int randEnemy = range1[random.Next(range1.Length)]; // Random from the array...
+            // we can select a random index, since range is an int[], by Random.Range(0, range.length), then we pass this selected index i to the range int[i] and this is our randEnemy
+            int[] range2 = Enumerable.Range(0, spawnPoints.Length).ToArray();
+            int randSpawnPoint = range2[random.Next(range2.Length)];
+
             Instantiate(BEAN[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
             spawns++;
         }
